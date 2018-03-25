a499c86cea8237463ff0eb4b333b18cbf1a8ea4f
Robin Hübner <profan@prfn.se>
2018-03-25 19:25:42 +0200

    we got us some level names too

--- level.gd
+++ level.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 export(String) var next_level
+export(String) var level_title
 
 onready var level_map = get_node("level_map")
 onready var modulator = get_node("modulator")
 onready var ui_modulator = get_node("canvas/ui_mod")
 onready var mod_tween = get_node("mod_tween")
+onready var level_title_label = get_node("canvas/level_title")
 
 var from_scene_name = false
 var next_scene_name = false
@@ -23,17 +25,28 @@ func _ready():
 	Game.start_level()
 	
 	modulator.color.a = 0
-	mod_tween.connect("tween_completed", self, "_on_mod_tween_load_end")
+	mod_tween.connect("tween_completed", self, "_on_mod_tween_load_end", [], CONNECT_ONESHOT)
 	mod_tween.interpolate_property(modulator, "color", Color(1, 1, 1, 0), Color(1, 1, 1, 1), SCENE_SWITCH_TIME, Tween.TRANS_CUBIC, Tween.EASE_IN)
 	mod_tween.interpolate_property(ui_modulator, "color", Color(1, 1, 1, 0), Color(1, 1, 1, 1), SCENE_SWITCH_TIME, Tween.TRANS_CUBIC, Tween.EASE_IN)
 	mod_tween.start()
 	
+	if level_title: level_title_label.text = level_title
+	
 	if next_level: next_scene_name = "res://levels/%s.tscn" % next_level
 
 func _on_mod_tween_load_end(obj, key):
-	tweens_done += 1
-	if tweens_done == 2:
-		mod_tween.connect("tween_completed", self, "_on_mod_tween_end_level")
+	mod_tween.interpolate_property(level_title_label, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), SCENE_SWITCH_TIME * 2, Tween.TRANS_CUBIC,Tween.EASE_IN)
+	mod_tween.connect("tween_completed", self, "_on_mod_tween_fade_title_in", [], CONNECT_ONESHOT)
+	mod_tween.start()
+
+func _on_mod_tween_fade_title_in(obj, key):
+	print("YAH")
+	mod_tween.interpolate_property(level_title_label, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), SCENE_SWITCH_TIME * 2, Tween.TRANS_CUBIC,Tween.EASE_IN)
+	mod_tween.connect("tween_completed", self, "_on_mod_tween_fade_title_out", [], CONNECT_ONESHOT)
+	mod_tween.start()
+
+func _on_mod_tween_fade_title_out(obj, key):
+	level_title_label.visible = false
 
 func _on_mod_tween_end_level(obj, key):
 	
