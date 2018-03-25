e8a5ec28d0a4586a9d9135421c27dd3f9abf2e79
Robin Hübner <profan@prfn.se>
2018-03-25 18:05:39 +0200

    fade in levels

--- level.gd
+++ level.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 onready var level_map = get_node("level_map")
+onready var modulator = get_node("modulator")
+onready var mod_tween = get_node("mod_tween")
 
 var from_scene_name = false
+var next_scene_name = false
+var SCENE_SWITCH_TIME = 0.5
 
 func _ready():
 	
@@ -10,6 +14,24 @@ func _ready():
 	
 	# init shit
 	Game.start_level()
+	
+	modulator.color.a = 0
+	mod_tween.connect("tween_completed", self, "_on_mod_tween_load_end")
+	mod_tween.interpolate_property(modulator, "color", Color(1, 1, 1, 0), Color(1, 1, 1, 1), SCENE_SWITCH_TIME, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	mod_tween.start()
+
+func _on_mod_tween_load_end():
+	mod_tween.connect("tween_completed", self, "_on_mod_tween_end_level")
+
+func _on_mod_tween_end_level():
+	SceneSwitcher.goto_scene(next_scene_name)
+	
+func end_level():
+	mod_tween.interpolate_property(modulator, "color", Color(1, 1, 1, 1), Color(1, 1, 1, 0), SCENE_SWITCH_TIME, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	mod_tween.start()
+
+func on_load_level():
+	pass
 
 func pos_has_tile(pos):
 	return level_map.pos_has_tile(pos)
